47d14ea7253a66984807c0281e22dcc40884d542
linvegas <saulo.costam@gmail.com>
2024-08-26 22:51:04 -0300

    Adding max and min parameters for generation.

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const VOWELS = "aeiou";
 const CONSONANTS = "bcdfghjklmnpqrstvxyz";
-const MAX_WORDS = 10;
 const MAX_SYL = 3;
 
 const genBtnElem = document.querySelector("button#gen-btn");
+const wordNumInputElem = document.querySelector("input#words-num-input");
+const sylMinElem = document.querySelector("input#syl-min");
+const sylMaxElem = document.querySelector("input#syl-max");
 const wordListElem = document.querySelector("ul#word-list");
 
 const print = (msg) => console.log(msg);
-
 const prob = (prob) => Math.random() * 100 < prob ? true : false;
+
+/**
+ * @typedef {Object} Options
+ * @property {number} [wordsLen=10]
+ * @property {Object} sylRange
+ * @property {number} sylRange.min
+ * @property {number} sylRange.max
+ */
+let options = {
+    wordsLen: 10,
+    sylRange: {
+        min: 2,
+        max: 3,
+    },
+}
+
+wordNumInputElem.value = options.wordsLen;
+sylMinElem.value = options.sylRange.min;
+sylMaxElem.value = options.sylRange.max;
 
 /** @param {Array<String>} list */
 function updateWordListElem(list) {
@@ -22,14 +42,18 @@ function updateWordListElem(list) {
     })
 }
 
-/** @returns {Array<String>} */
-function generateWordsList() {
+/**
+ * @param {Options}
+ * @returns {Array<String>}
+ * */
+function generateWordsList({wordsLen, sylRange}) {
     let list = [];
 
-    for (let i = 0; i < MAX_WORDS; i++) {
+    for (let i = 0; i < wordsLen; i++) {
         let word = "";
+        let sylLen = Math.floor(Math.random() * (sylRange.max - sylRange.min + 1)) + sylRange.min;
 
-        for (let i = 0; i < (Math.floor(Math.random()*MAX_SYL) + 2); i++) {
+        for (let i = 0; i < sylLen; i++) {
             let vow = VOWELS[Math.floor(Math.random()*VOWELS.length)];
             let con = CONSONANTS[Math.floor(Math.random()*CONSONANTS.length)];
 
@@ -41,6 +65,8 @@ function generateWordsList() {
                 syl += 'm';
             } else if (prob(14)) {
                 syl += 'n';
+            } else if (prob(20)) {
+                syl += 'r';
             } else if (prob(18)) {
                 syl += 'l';
             } else if (prob(7)) {
@@ -57,10 +83,19 @@ function generateWordsList() {
 }
 
 genBtnElem.addEventListener("click", () => {
-    // event.preventDefault();
-    let wordList = generateWordsList();
+    let wordList = generateWordsList(options);
     updateWordListElem(wordList)
     wordList = [];
 });
 
+wordNumInputElem.addEventListener("input", event => {
+    options.wordsLen = parseInt(event.target.value);
+});
 
+sylMinElem.addEventListener("input", event => {
+    options.sylRange.min = parseInt(event.target.value);
+});
+
+sylMaxElem.addEventListener("input", event => {
+    options.sylRange.max = parseInt(event.target.value);
+});
